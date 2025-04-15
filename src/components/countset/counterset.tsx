05890b17d470dc3bd494d0abc66c1984755b9daf
Harrison John <harrisjohnu@gmail.com>
2025-04-15 21:05:43 +0000

    chore(CAUK-3009): deleted unnecessary files

--- src/components/countset/counterset.tsx
+++
@@ -1,7 +0,0 @@
-import { userCount } from "@/app/prices/prices";
-import { useAtom } from "jotai";
-
-export default async function CountSetter() {
-    const [count, setCount] = useAtom(userCount);
-    const countValue = await usercounter()
-}
