56c3e9e144d92e7417100f9176c4e6e00c354fd0
Harrison John <harrisjohnu@gmail.com>
2025-04-15 21:46:53 +0000

    fix(CAUK-3009): straightened up users display

--- src/app/dashboard/components/container/container.tsx
+++ src/app/dashboard/components/container/container.tsx
@@ -51,7 +51,7 @@ ChartJS.register(
   LinearScale,
   PointElement,
   Tooltip,
-  Legend
+  Legend,
 );
 
 export default function AgriculturalDashboard() {
@@ -84,7 +84,7 @@ export default function AgriculturalDashboard() {
       education: "",
       country: "",
       address: "",
-    }
+    },
   );
   const [drop, setDrop] = useState(false);
   const session = useSession();
@@ -104,7 +104,7 @@ export default function AgriculturalDashboard() {
     const fetchUsers = async () => {
       const response = await TotalUsers();
       const userCountry = data.find(
-        (country) => country.country === session.data?.user.country
+        (country) => country.country === session.data?.user.country,
       );
       if (userCountry && userCountry.price) {
         setPrice(userCountry.price);
